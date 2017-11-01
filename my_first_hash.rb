0e09fb423bdb5a87a643b785d5c6a04df6d4f90f
Laura Trager <laura.trager@gmail.com>
2017-11-01 17:52:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,9 +9,10 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
-def adding
+def adding(shipping_manifest)
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["oil paintings"]
+
 end
